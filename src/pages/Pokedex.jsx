a85e579ddc7cd29929a116df8f96419528206be4
federicocoletto <federico.coletto@fce.uncu.edu.ar>
2023-06-19 11:47:54 -0300

    ww

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -3,26 +3,47 @@ import { useEffect } from "react";
 import useFetch from "../hooks/useFetch";
 import { useSelector } from "react-redux";
 import PokeContainer from "../components/Pokedex/PokeContainer";
+import Select from "react-select";
 
 const Pokedex = () => {
 
-    const base_URL = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0'
-    const { trainerName } = useSelector(states => states)
+	const base_URL = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0'
+	const { trainerName } = useSelector(states => states)
 
 
-    const [pokemons, getAllPokemons] = useFetch(base_URL)
+	const [pokemons, getAllPokemons] = useFetch(base_URL)
+	let names = []
 
-    useEffect(() => {
-        getAllPokemons()
-    }, []);
-    
+	useEffect(() => {
+		getAllPokemons()
+	}, []);
+	
+	
+	pokemons?.results.map(pokemon => {
+		names.push({
+			label: pokemon.name,
+			value: pokemon.name,
+		})
+	})
 
-    return (
-        <>
-            <h1>Welcome {trainerName}, here you'll find your favourite pokemons</h1>
-            <PokeContainer pokemons={pokemons?.results} />
-        </>
-    );
+	const handleChange = (e) => {
+		console.log(e);
+	}
+	
+	const handleSubmit = (e) => {
+		e.preventDefault()
+	}
+
+	return (
+		<>
+			<h1>Welcome {trainerName}, here you'll find your favourite pokemons</h1>
+			<form onSubmit={handleSubmit}>
+				<Select onChange={handleChange} options={names} />
+				<button>d</button>
+			</form>
+			<PokeContainer pokemons={pokemons?.results} />
+		</>
+	);
 };
 
 export default Pokedex;
